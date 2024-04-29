974f152e6a7268486c3e09d818f7f00166869361
spookyorange <korkmazcoltu@mail.ru>
2024-04-29 23:07:10 +0300

    fixed a bug about not showing app name on uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 app_name="tetrio"
+display_name="Tetr.IO"
 literal_name_of_installation_directory=".tarball-installations"
 general_installation_directory="$HOME/$literal_name_of_installation_directory"
 application_installation_directory="$general_installation_directory/$app_name"
@@ -10,7 +11,7 @@ app_bin_in_local_bin="$local_bin_path/$app_name"
 desktop_in_local_applications="$local_application_path/$app_name.desktop"
 
 
-echo "Removing the installation of $application_name from your home"
+echo "Removing the installation of $display_name from your home"
 rm -rf $application_installation_directory
 rm $app_bin_in_local_bin
 rm $desktop_in_local_applications
